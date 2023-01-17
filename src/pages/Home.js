8843ee0ab2be182892c4484826b14531494ee81c
Peter Hui <huishikuen@gmail.com>
2023-01-17 11:13:53 -0800

    Setup react router

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 import React from "react"
+import { useNavigate } from 'react-router-dom';
 import { TextareaField } from "evergreen-ui"
+import NavBar from "../components/NavBar";
 
 const textareaFieldsConfig = [
     { label: "Resume", required: true },
@@ -8,8 +10,10 @@ const textareaFieldsConfig = [
 ];
 
 function Home() {
+    const navigate = useNavigate();
     return (
         <>
+            <NavBar />
             <div className="grid grid-cols-1 lg:grid-cols-2 mt-6">
                 {textareaFieldsConfig.map(({ label, required }, i) => (
                     <div key={i} className="px-6">
@@ -22,7 +26,10 @@ function Home() {
                 ))}
             </div>
             <div className="flex justify-center">
-                <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold mt-4 py-2 px-4 border border-gray-400 rounded shadow">
+                <button 
+                    className="bg-white hover:bg-gray-100 text-gray-800 font-semibold mt-4 py-2 px-4 border border-gray-400 rounded shadow"
+                    onClick={() => navigate("result")}
+                >
                     Generate Cover Letter
                 </button>
             </div>
---
+++ src/pages/Result.js
@@ -0,0 +1,13 @@
+import React from "react"
+import NavBar from "../components/NavBar";
+
+function Result() {
+    return (
+        <>
+            <NavBar />
+            <div>This is the page to display the cover letter (to be updated)</div>
+        </>
+    );
+}
+
+export default Result;
