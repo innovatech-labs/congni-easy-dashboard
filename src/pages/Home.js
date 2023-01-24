c83805d159b05178bbc30fff6186df5254a6df52
Peter Hui <huishikuen@gmail.com>
2023-01-23 16:37:00 -0800

    Place sub title to center

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { useNavigate } from 'react-router-dom';
 import { TextareaField } from "evergreen-ui"
 
 const textareaFieldsConfig = [
@@ -9,13 +8,12 @@ const textareaFieldsConfig = [
 ];
 
 function Home() {
-    const navigate = useNavigate();
     return (
         <div className='flex flex-col items-center min-h-screen'>
             <div className='text-6xl font-bold my-6' id="app-title">
                 Innovatech
             </div>
-            <p className='mt-1 text-2xl text-slate-500'>Create quality cover letter with AI technology</p>
+            <p className='mt-1 text-2xl text-slate-500 text-center'>Create quality cover letter with AI technology</p>
             <div className='max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-3'>
                 <form className='w-full'>
                     <div className="grid grid-cols-1 lg:grid-cols-2 mt-6">
