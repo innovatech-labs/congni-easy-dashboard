2b6ea44e9e5ca7582ffa2f67e4208d8e44007753
Peter Hui <huishikuen@gmail.com>
2023-01-23 16:45:47 -0800

    Add modal to show result

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
-import React from "react"
+import React, { useState } from "react"
 import { TextareaField } from "evergreen-ui"
+import ResultModal from "../components/ResultModal";
 
 const textareaFieldsConfig = [
     { label: "Resume", required: true },
@@ -8,33 +9,44 @@ const textareaFieldsConfig = [
 ];
 
 function Home() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
     return (
-        <div className='flex flex-col items-center min-h-screen'>
-            <div className='text-6xl font-bold my-6' id="app-title">
-                Innovatech
+        <>
+            <div className='flex flex-col items-center min-h-screen'>
+                <div className='text-6xl font-bold my-6' id="app-title">
+                    Innovatech
+                </div>
+                <p className='mt-1 text-2xl text-slate-500 text-center'>Create quality cover letter with AI technology</p>
+                <div className='max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-3'>
+                    <form className='w-full'>
+                        <div className="grid grid-cols-1 lg:grid-cols-2 mt-6">
+                            {textareaFieldsConfig.map(({ label, required }, i) => (
+                                <div key={i} className="px-6">
+                                    <TextareaField
+                                        label={label}
+                                        required={required}
+                                        style={{ minHeight: "200px" }}
+                                    />
+                                </div>
+                            ))}
+                        </div>
+                    </form>
+                </div>
+                <div className="flex justify-center">
+                    <button 
+                        className="bg-emerald-500 hover:bg-emerald-600 text-white font-semibold mt-4 py-2 px-4 border rounded-full shadow"
+                        onClick={() => setIsModalOpen(true)}
+                    >
+                        Generate
+                    </button>
+                </div>
             </div>
-            <p className='mt-1 text-2xl text-slate-500 text-center'>Create quality cover letter with AI technology</p>
-            <div className='max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-3'>
-                <form className='w-full'>
-                    <div className="grid grid-cols-1 lg:grid-cols-2 mt-6">
-                        {textareaFieldsConfig.map(({ label, required }, i) => (
-                            <div key={i} className="px-6">
-                                <TextareaField
-                                    label={label}
-                                    required={required}
-                                    style={{ minHeight: "200px" }}
-                                />
-                            </div>
-                        ))}
-                    </div>
-                </form>
-            </div>
-            <div className="flex justify-center">
-                <button className="bg-emerald-500 hover:bg-emerald-600 text-white font-semibold mt-4 py-2 px-4 border rounded-full shadow">
-                    Generate
-                </button>
-            </div>
-        </div>
+            <ResultModal 
+                isOpen={isModalOpen} 
+                setIsOpen={setIsModalOpen}
+            />
+        </>
     );
 }
 
