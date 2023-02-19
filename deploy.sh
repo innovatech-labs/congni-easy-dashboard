dd0fd475a844076fc3abff64cd41d0d3e6f903f1
Peter Hui <huishikuen@gmail.com>
2023-02-18 20:01:24 -0800

    Update deployment script

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,16 @@ cd congni-easy-dashboard
 # TO BE DELETED
 git checkout deploy-to-gh-pages
 
+# Configure remote origin
+git remote rm origin
+git remote add origin https://github.com/innovatech-labs/congni-easy-deployment-test.git
+
 echo "Installing dependencies ..."
 yarn --silent
 
 echo "Deploying ..."
 yarn run deploy
 
-echo "REMEBER TO MANUALLY DELETE THE DIRECTORY OF congni-easy-dashboard"
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+printf "\n\n${RED}REMEBER TO MANUALLY DELETE THE DIRECTORY OF congni-easy-dashboard${NC}\n\n"
