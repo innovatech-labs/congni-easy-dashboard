3e8b03f97773047eaf33c4bfe90bb4f2fb185b78
Peter Hui <huishikuen@gmail.com>
2023-04-24 22:45:30 -0700

    Add headers

--- src/utils/apiHelper.js
+++ src/utils/apiHelper.js
@@ -3,6 +3,9 @@ const API_ENDPOINT = process.env.REACT_APP_API_URL ?? "http://127.0.0.1:8000";
 export const callApi = async({resumeInput, jobDescriptionInput, coverLetterInput}) => {
     const res = await fetch(`${API_ENDPOINT}/cover_letter`, {
         method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
         body: JSON.stringify({
             "resume": resumeInput,
             "job_posting": jobDescriptionInput,
